b3767f48a9f5f571e3b03c40e16c6ffd7946f2eb
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-04-17 23:12:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ app=Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_templates("index.html", title="Hello")
+
+@app.route("/home")
+def home():
+    return render_templates("home.html")
