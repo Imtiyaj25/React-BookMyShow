db15b7ff77af53a5dc2bf19d1b774e1a4ffa028c
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-09-01 22:33:38 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_templates
-app=Flask(__name__)
-@app.route("/")
-def hello_world():
-    return render_templates("index.html", title="Hello")
-
-@app.route("/home")
-def home():
-    return render_templates("home.html")
