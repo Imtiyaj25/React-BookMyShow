9261cc0548868360519b8e3319b5c14e3dd0cbb7
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2023-09-03 23:32:46 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 import './About.css'
 import Card1 from '../Components/Card1'
 const About = () => {
+  let preveiwContainer = document.querySelector('.products-preview');
+  let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+  document.querySelectorAll('.products-container .product').forEach(product => {
+    product.onclick = () => {
+      preveiwContainer.style.display = 'flex';
+      let name = product.getAttribute('data-name');
+      previewBox.forEach(preview => {
+        let target = preview.getAttribute('data-target');
+        if (name == target) {
+          preview.classList.add('active');
+        }
+      });
+    };
+  });
+  previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+      close.classList.remove('active');
+      preveiwContainer.style.display = 'none';
+    };
+  });
   return (
     <>
       <div class="card-container">
