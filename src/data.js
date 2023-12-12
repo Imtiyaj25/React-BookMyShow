869c73ec64543b801dc59dcd3f485cde94812925
Imtiyaj Sayyad <imsayyad97@gmail.com>
2023-12-12 20:49:06 +0530

    Updating Movies

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-let movies = ['Suraj par mangal bhari', 'Tenet', 'The war with grandpa', 'The personal history of David Copperfield', 'Come Play']
+let movies = ['Shazam', 'Winter Soldier', 'Flash', 'SpiderMan', 'GrayMan']
 let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"]
 let seatTypes = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2']
 
